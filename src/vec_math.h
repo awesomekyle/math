762bfc8cdc02fce9423d00682de98b675451acd5
Kyle Weicht <kyle@awesomekyle.com>
2013-03-28 15:32:19 -0700

    Lots of math vector work

--- src/vec_math.h
+++ src/vec_math.h
@@ -13,6 +13,18 @@ typedef struct Vec2
 {
     float x, y;
 } Vec2;
+typedef struct Vec3
+{
+    float x, y, z;
+} Vec3;
+typedef struct Vec4
+{
+    float x, y, z, w;
+} Vec4;
+typedef struct Mat3
+{
+    Vec3 r0, r1, r2;
+} Mat3;
 
 /**
  * Constants
@@ -28,15 +40,22 @@ static const float kRadToDeg = 57.29577951308232087679815481410f;
 #ifdef __cplusplus
 extern "C" { // C linkage
     typedef const Vec2& VEC2_INPUT;
+    typedef const Vec3& VEC3_INPUT;
+    typedef const Vec4& VEC4_INPUT;
+    typedef const Mat3& MAT3_INPUT;
     #define INLINE inline
 #else
     typedef Vec2 VEC2_INPUT;
+    typedef Vec3 VEC3_INPUT;
+    typedef Vec4 VEC4_INPUT;
+    typedef Mat3 MAT3_INPUT;
     #define INLINE static __inline
 #endif
 
 /******************************************************************************\
  * Vec2                                                                       *
 \******************************************************************************/
+static const Vec2 vec2_zero = {0.0f,0.0f};
 /* Basic aritmatic */
 INLINE Vec2 vec2_add(VEC2_INPUT a, VEC2_INPUT b)
 {
@@ -144,6 +163,342 @@ INLINE Vec2 vec2_max(VEC2_INPUT a, VEC2_INPUT b)
     v.x = fmaxf(a.x, b.x);
     v.y = fmaxf(a.y, b.y);
     return v;
+}
+INLINE Vec2 vec2_lerp(VEC2_INPUT a, VEC2_INPUT b, float t)
+{
+    Vec2 d = vec2_sub(b, a);
+    Vec2 tt = vec2_mul_scalar(d, t);
+    return vec2_add(a, tt);
+}
+INLINE Vec2 vec2_negate(VEC2_INPUT v)
+{
+    Vec2 r;
+    r.x = -v.x;
+    r.y = -v.y;
+    return r;
+}
+
+
+/******************************************************************************\
+ * Vec3                                                                       *
+\******************************************************************************/
+static const Vec3 vec3_zero = {0.0f,0.0f,0.0f};
+/* Basic aritmatic */
+INLINE Vec3 vec3_add(VEC3_INPUT a, VEC3_INPUT b)
+{
+    Vec3 res;
+    res.x = a.x + b.x;
+    res.y = a.y + b.y;
+    res.z = a.z + b.z;
+    return res;
+}
+INLINE Vec3 vec3_sub(VEC3_INPUT a, VEC3_INPUT b)
+{
+    Vec3 res;
+    res.x = a.x - b.x;
+    res.y = a.y - b.y;
+    res.z = a.z - b.z;
+    return res;
+}
+INLINE Vec3 vec3_mul(VEC3_INPUT a, VEC3_INPUT b)
+{
+    Vec3 res;
+    res.x = a.x * b.x;
+    res.y = a.y * b.y;
+    res.z = a.z * b.z;
+    return res;
+}
+INLINE Vec3 vec3_div(VEC3_INPUT a, VEC3_INPUT b)
+{
+    Vec3 res;
+    res.x = a.x / b.x;
+    res.y = a.y / b.y;
+    res.z = a.z / b.z;
+    return res;
+}
+
+/* Scalar math */
+INLINE Vec3 vec3_add_scalar(VEC3_INPUT v, float f)
+{
+    Vec3 res;
+    res.x = v.x + f;
+    res.y = v.y + f;
+    res.z = v.z + f;
+    return res;
+}
+INLINE Vec3 vec3_sub_scalar(VEC3_INPUT v, float f)
+{
+    Vec3 res;
+    res.x = v.x - f;
+    res.y = v.y - f;
+    res.z = v.z - f;
+    return res;
+}
+INLINE Vec3 vec3_mul_scalar(VEC3_INPUT v, float f)
+{
+    Vec3 res;
+    res.x = v.x * f;
+    res.y = v.y * f;
+    res.z = v.z * f;
+    return res;
+}
+INLINE Vec3 vec3_div_scalar(VEC3_INPUT v, float f)
+{
+    Vec3 res;
+    res.x = v.x / f;
+    res.y = v.y / f;
+    res.z = v.z / f;
+    return res;
+}
+/* Misc */
+INLINE float vec3_hadd(VEC3_INPUT v)
+{
+    return v.x + v.y + v.z;
+}
+INLINE int vec3_equal(VEC3_INPUT a, VEC3_INPUT b)
+{
+    return fabsf(a.x - b.x) < kEpsilon &&
+           fabsf(a.y - b.y) < kEpsilon &&
+           fabsf(a.z - b.z) < kEpsilon;
+}
+INLINE int vec3_equal_scalar(VEC3_INPUT v, float f)
+{
+    return fabsf(v.x - f) < kEpsilon &&
+           fabsf(v.y - f) < kEpsilon &&
+           fabsf(v.z - f) < kEpsilon;
+}
+INLINE float vec3_length_sq(VEC3_INPUT v)
+{
+    return v.x*v.x + v.y*v.y + v.z*v.z;
+}
+INLINE float vec3_length(VEC3_INPUT v)
+{
+    return sqrtf(vec3_length_sq(v));
+}
+INLINE float vec3_distance_sq(VEC3_INPUT a, VEC3_INPUT b)
+{
+    return vec3_length_sq(vec3_sub(a,b));
+}
+INLINE float vec3_distance(VEC3_INPUT a, VEC3_INPUT b)
+{
+    return sqrtf(vec3_distance_sq(a,b));
+}
+INLINE Vec3 vec3_normalize(VEC3_INPUT v)
+{
+    return vec3_div_scalar(v,vec3_length(v));
+}
+INLINE Vec3 vec3_min(VEC3_INPUT a, VEC3_INPUT b)
+{
+    Vec3 v;
+    v.x = fminf(a.x, b.x);
+    v.y = fminf(a.y, b.y);
+    v.z = fminf(a.z, b.z);
+    return v;
+}
+INLINE Vec3 vec3_max(VEC3_INPUT a, VEC3_INPUT b)
+{
+    Vec3 v;
+    v.x = fmaxf(a.x, b.x);
+    v.y = fmaxf(a.y, b.y);
+    v.z = fmaxf(a.z, b.z);
+    return v;
+}
+INLINE Vec3 vec3_lerp(VEC3_INPUT a, VEC3_INPUT b, float t)
+{
+    Vec3 d = vec3_sub(b, a);
+    Vec3 tt = vec3_mul_scalar(d, t);
+    return vec3_add(a, tt);
+}
+INLINE Vec3 vec3_negate(VEC3_INPUT v)
+{
+    Vec3 r;
+    r.x = -v.x;
+    r.y = -v.y;
+    r.z = -v.z;
+    return r;
+}
+INLINE float vec3_dot(VEC3_INPUT a, VEC3_INPUT b)
+{
+    return a.x*b.x + a.y*b.y + a.z*b.z;
+}
+INLINE Vec3 vec3_cross(VEC3_INPUT a, VEC3_INPUT b)
+{
+    Vec3 r;
+    r.x = a.y*b.z - a.z*b.y;
+    r.y = a.z*b.x - a.x*b.z;
+    r.z = a.x*b.y - a.y*b.x;
+    return r;
+}
+
+
+
+/******************************************************************************\
+ * Vec4                                                                       *
+\******************************************************************************/
+static const Vec4 vec4_zero = {0.0f,0.0f,0.0f,0.0f};
+/* Basic aritmatic */
+INLINE Vec4 vec4_add(VEC4_INPUT a, VEC4_INPUT b)
+{
+    Vec4 res;
+    res.x = a.x + b.x;
+    res.y = a.y + b.y;
+    res.z = a.z + b.z;
+    res.w = a.w + b.w;
+    return res;
+}
+INLINE Vec4 vec4_sub(VEC4_INPUT a, VEC4_INPUT b)
+{
+    Vec4 res;
+    res.x = a.x - b.x;
+    res.y = a.y - b.y;
+    res.z = a.z - b.z;
+    res.w = a.w - b.w;
+    return res;
+}
+INLINE Vec4 vec4_mul(VEC4_INPUT a, VEC4_INPUT b)
+{
+    Vec4 res;
+    res.x = a.x * b.x;
+    res.y = a.y * b.y;
+    res.z = a.z * b.z;
+    res.w = a.w * b.w;
+    return res;
+}
+INLINE Vec4 vec4_div(VEC4_INPUT a, VEC4_INPUT b)
+{
+    Vec4 res;
+    res.x = a.x / b.x;
+    res.y = a.y / b.y;
+    res.z = a.z / b.z;
+    res.w = a.w / b.w;
+    return res;
+}
+
+/* Scalar math */
+INLINE Vec4 vec4_add_scalar(VEC4_INPUT v, float f)
+{
+    Vec4 res;
+    res.x = v.x + f;
+    res.y = v.y + f;
+    res.z = v.z + f;
+    res.w = v.w + f;
+    return res;
+}
+INLINE Vec4 vec4_sub_scalar(VEC4_INPUT v, float f)
+{
+    Vec4 res;
+    res.x = v.x - f;
+    res.y = v.y - f;
+    res.z = v.z - f;
+    res.w = v.w - f;
+    return res;
+}
+INLINE Vec4 vec4_mul_scalar(VEC4_INPUT v, float f)
+{
+    Vec4 res;
+    res.x = v.x * f;
+    res.y = v.y * f;
+    res.z = v.z * f;
+    res.w = v.w * f;
+    return res;
+}
+INLINE Vec4 vec4_div_scalar(VEC4_INPUT v, float f)
+{
+    Vec4 res;
+    res.x = v.x / f;
+    res.y = v.y / f;
+    res.z = v.z / f;
+    res.w = v.w / f;
+    return res;
+}
+/* Misc */
+INLINE float vec4_hadd(VEC4_INPUT v)
+{
+    return v.x + v.y + v.z + v.w;
+}
+INLINE int vec4_equal(VEC4_INPUT a, VEC4_INPUT b)
+{
+    return fabsf(a.x - b.x) < kEpsilon &&
+           fabsf(a.y - b.y) < kEpsilon &&
+           fabsf(a.z - b.z) < kEpsilon &&
+           fabsf(a.w - b.w) < kEpsilon;
+}
+INLINE int vec4_equal_scalar(VEC4_INPUT v, float f)
+{
+    return fabsf(v.x - f) < kEpsilon &&
+           fabsf(v.y - f) < kEpsilon &&
+           fabsf(v.z - f) < kEpsilon &&
+           fabsf(v.w - f) < kEpsilon;
+}
+INLINE float vec4_length_sq(VEC4_INPUT v)
+{
+    return v.x*v.x + v.y*v.y + v.z*v.z + v.w*v.w;
+}
+INLINE float vec4_length(VEC4_INPUT v)
+{
+    return sqrtf(vec4_length_sq(v));
+}
+INLINE float vec4_distance_sq(VEC4_INPUT a, VEC4_INPUT b)
+{
+    return vec4_length_sq(vec4_sub(a,b));
+}
+INLINE float vec4_distance(VEC4_INPUT a, VEC4_INPUT b)
+{
+    return sqrtf(vec4_distance_sq(a,b));
+}
+INLINE Vec4 vec4_normalize(VEC4_INPUT v)
+{
+    return vec4_div_scalar(v,vec4_length(v));
+}
+INLINE Vec4 vec4_min(VEC4_INPUT a, VEC4_INPUT b)
+{
+    Vec4 v;
+    v.x = fminf(a.x, b.x);
+    v.y = fminf(a.y, b.y);
+    v.z = fminf(a.z, b.z);
+    v.w = fminf(a.w, b.w);
+    return v;
+}
+INLINE Vec4 vec4_max(VEC4_INPUT a, VEC4_INPUT b)
+{
+    Vec4 v;
+    v.x = fmaxf(a.x, b.x);
+    v.y = fmaxf(a.y, b.y);
+    v.z = fmaxf(a.z, b.z);
+    v.w = fmaxf(a.w, b.w);
+    return v;
+}
+INLINE Vec4 vec4_lerp(VEC4_INPUT a, VEC4_INPUT b, float t)
+{
+    Vec4 d = vec4_sub(b, a);
+    Vec4 tt = vec4_mul_scalar(d, t);
+    return vec4_add(a, tt);
+}
+INLINE Vec4 vec4_negate(VEC4_INPUT v)
+{
+    Vec4 r;
+    r.x = -v.x;
+    r.y = -v.y;
+    r.z = -v.z;
+    r.w = -v.w;
+    return r;
+}
+
+/******************************************************************************\
+ * Mat3                                                                       *
+\******************************************************************************/
+static const Mat3 mat3_identity = {
+    { 1.0f, 0.0f, 0.0f },
+    { 0.0f, 1.0f, 0.0f },
+    { 0.0f, 0.0f, 1.0f },
+};
+INLINE Mat3 mat3_scale(float x, float y, float z)
+{
+    Mat3 r = mat3_identity;
+    r.r0.x = x;
+    r.r1.y = y;
+    r.r2.z = z;
+    return r;
 }
 
 #ifdef __cplusplus
--- test/math_test_glm.cpp
+++ test/math_test_glm.cpp
@@ -5,13 +5,37 @@
 
 #include "vec_math.h"
 #include "glm/glm.hpp"
+#include "glm/gtc/quaternion.hpp"
+#include "glm/gtc/matrix_transform.hpp"
 
 #define CHECK_EQUAL_VEC2(a,b) \
     CHECK_EQUAL_FLOAT((a)[0], (b)[0]); \
     CHECK_EQUAL_FLOAT((a)[1], (b)[1])
 
+#define CHECK_EQUAL_VEC3(a,b) \
+    CHECK_EQUAL_FLOAT((a)[0], (b)[0]); \
+    CHECK_EQUAL_FLOAT((a)[1], (b)[1]); \
+    CHECK_EQUAL_FLOAT((a)[2], (b)[2])
+
+#define CHECK_EQUAL_VEC4(a,b) \
+    CHECK_EQUAL_FLOAT((a)[0], (b)[0]); \
+    CHECK_EQUAL_FLOAT((a)[1], (b)[1]); \
+    CHECK_EQUAL_FLOAT((a)[2], (b)[2]); \
+    CHECK_EQUAL_FLOAT((a)[3], (b)[3])
+
+#define CHECK_EQUAL_MAT3(expected, actual)  \
+    for(int ii=0;ii<9;++ii) {                   \
+        CHECK_EQUAL_FLOAT((expected)[ii], (actual)[ii]);    \
+    }
+
 namespace
 {
+
+template<typename T>
+inline T glm_lerp(const T& a, const T& b, float t)
+{
+    return a + (b-a)*t;
+}
 
 float _rand_float(float min, float max)
 {
@@ -20,6 +44,9 @@ float _rand_float(float min, float max)
     return f-min;
 }
 
+/******************************************************************************\
+ * Vec2                                                                       *
+\******************************************************************************/
 struct Vec2Fixture
 {
     glm::vec2   a,b,c;
@@ -30,7 +57,7 @@ struct Vec2Fixture
     {
         float* _a = (float*)&a;
         float* _b = (float*)&i;
-        float* end = _a + 2*3;
+        float* end = _a +  sizeof(a)/sizeof(float) * 3;
         while(_a != end) {
             *_b = *_a = _rand_float(-100.0f, 100.0f);
             ++_a, ++_b;
@@ -39,7 +66,12 @@ struct Vec2Fixture
     }
     ~Vec2Fixture() { }
 };
-
+TEST_FIXTURE(Vec2Fixture, Vec2Zero)
+{
+    a = glm::vec2(0.0f);
+    i = vec2_zero;
+    CHECK_EQUAL_VEC2((float*)&a, (float*)&i);
+}
 TEST_FIXTURE(Vec2Fixture, Vec2Add)
 {
     a = b+c;
@@ -126,6 +158,327 @@ TEST_FIXTURE(Vec2Fixture, Vec2MinMax)
 }
 TEST_FIXTURE(Vec2Fixture, Vec2Lerp)
 {
+    float f = _rand_float(0.0f, 1.0f);
+    a = glm_lerp(b,c,f);
+    i = vec2_lerp(j,k,f);
+    CHECK_EQUAL_VEC2((float*)&a, (float*)&i);
+}
+TEST_FIXTURE(Vec2Fixture, Vec2Negate)
+{
+    a = -b;
+    i = vec2_negate(j);
+    CHECK_EQUAL_VEC2((float*)&a, (float*)&i);
+}
+
+
+/******************************************************************************\
+ * Vec3                                                                       *
+\******************************************************************************/
+struct Vec3Fixture
+{
+    glm::vec3   a,b,c;
+    Vec3        i,j,k;
+    float       s;
+
+    Vec3Fixture()
+    {
+        float* _a = (float*)&a;
+        float* _b = (float*)&i;
+        float* end = _a +  sizeof(a)/sizeof(float) * 3;
+        while(_a != end) {
+            *_b = *_a = _rand_float(-100.0f, 100.0f);
+            ++_a, ++_b;
+        }
+        s = _rand_float(-100.0f, 100.0f);
+    }
+    ~Vec3Fixture() { }
+};
+TEST_FIXTURE(Vec3Fixture, Vec3Zero)
+{
+    a = glm::vec3(0.0f);
+    i = vec3_zero;
+    CHECK_EQUAL_VEC3((float*)&a, (float*)&i);
+}
+TEST_FIXTURE(Vec3Fixture, Vec3Add)
+{
+    a = b+c;
+    i = vec3_add(j,k);
+    CHECK_EQUAL_VEC3((float*)&a, (float*)&i);
+}
+TEST_FIXTURE(Vec3Fixture, Vec3Sub)
+{
+    a = b-c;
+    i = vec3_sub(j,k);
+    CHECK_EQUAL_VEC3((float*)&a, (float*)&i);
+}
+TEST_FIXTURE(Vec3Fixture, Vec3Mul)
+{
+    a = b*c;
+    i = vec3_mul(j,k);
+    CHECK_EQUAL_VEC3((float*)&a, (float*)&i);
+}
+TEST_FIXTURE(Vec3Fixture, Vec3Div)
+{
+    a = b/c;
+    i = vec3_div(j,k);
+    CHECK_EQUAL_VEC3((float*)&a, (float*)&i);
+}
+
+TEST_FIXTURE(Vec3Fixture, Vec3AddScalar)
+{
+    a = b+s;
+    i = vec3_add_scalar(j,s);
+    CHECK_EQUAL_VEC3((float*)&a, (float*)&i);
+}
+TEST_FIXTURE(Vec3Fixture, Vec3SubScalar)
+{
+    a = b-s;
+    i = vec3_sub_scalar(j,s);
+    CHECK_EQUAL_VEC3((float*)&a, (float*)&i);
+}
+TEST_FIXTURE(Vec3Fixture, Vec3MulScalar)
+{
+    a = b*s;
+    i = vec3_mul_scalar(j,s);
+    CHECK_EQUAL_VEC3((float*)&a, (float*)&i);
+}
+TEST_FIXTURE(Vec3Fixture, Vec3DivScalar)
+{
+    a = b/s;
+    i = vec3_div_scalar(j,s);
+    CHECK_EQUAL_VEC3((float*)&a, (float*)&i);
+}
+TEST_FIXTURE(Vec3Fixture, Vec3Hadd)
+{
+    float expected = a.x+a.y+a.z;
+    float actual = vec3_hadd(i);
+    CHECK_EQUAL_FLOAT(expected, actual);
+}
+TEST_FIXTURE(Vec3Fixture, Vec3Equal)
+{
+    CHECK_TRUE(vec3_equal(j, j));
+    CHECK_FALSE(vec3_equal(j, k));
+}
+TEST_FIXTURE(Vec3Fixture, Vec3Length)
+{
+    CHECK_EQUAL_FLOAT(glm::length(a), vec3_length(i));
+}
+TEST_FIXTURE(Vec3Fixture, Vec3Distance)
+{
+    CHECK_EQUAL_FLOAT(glm::distance(a,b), vec3_distance(i,j));
+    CHECK_EQUAL_FLOAT(vec3_distance(j,i), vec3_distance(i,j));
+}
+TEST_FIXTURE(Vec3Fixture, Vec3Normalize)
+{
+    a = glm::normalize(a);
+    i = vec3_normalize(i);
+    CHECK_EQUAL_VEC3((float*)&a, (float*)&i);
+}
+TEST_FIXTURE(Vec3Fixture, Vec3MinMax)
+{
+    a = glm::max(b, c);
+    i = vec3_max(j,k);
+    CHECK_EQUAL_VEC3((float*)&a, (float*)&i);
+    a = glm::min(b, c);
+    i = vec3_min(j,k);
+    CHECK_EQUAL_VEC3((float*)&a, (float*)&i);
+}
+TEST_FIXTURE(Vec3Fixture, Vec3Lerp)
+{
+    float f = _rand_float(0.0f, 1.0f);
+    a = glm_lerp(b,c,f);
+    i = vec3_lerp(j,k,f);
+    CHECK_EQUAL_VEC3((float*)&a, (float*)&i);
+}
+TEST_FIXTURE(Vec3Fixture, Vec3Negate)
+{
+    a = -b;
+    i = vec3_negate(j);
+    CHECK_EQUAL_VEC3((float*)&a, (float*)&i);
+}
+TEST_FIXTURE(Vec3Fixture, Vec3Dot)
+{
+    CHECK_EQUAL_FLOAT(glm::dot(a,b), vec3_dot(i,j));
+}
+TEST_FIXTURE(Vec3Fixture, Vec3Cross)
+{
+    a = glm::cross(b,c);
+    i = vec3_cross(j,k);
+    CHECK_EQUAL_VEC3((float*)&a, (float*)&i);
+}
+
+
+/******************************************************************************\
+ * Vec4                                                                       *
+\******************************************************************************/
+struct Vec4Fixture
+{
+    glm::vec4   a,b,c;
+    Vec4        i,j,k;
+    float       s;
+
+    Vec4Fixture()
+    {
+        float* _a = (float*)&a;
+        float* _b = (float*)&i;
+        float* end = _a +  sizeof(a)/sizeof(float) * 3;
+        while(_a != end) {
+            *_b = *_a = _rand_float(-100.0f, 100.0f);
+            ++_a, ++_b;
+        }
+        s = _rand_float(-100.0f, 100.0f);
+    }
+    ~Vec4Fixture() { }
+};
+TEST_FIXTURE(Vec4Fixture, Vec4Zero)
+{
+    a = glm::vec4(0.0f);
+    i = vec4_zero;
+    CHECK_EQUAL_VEC4((float*)&a, (float*)&i);
+}
+TEST_FIXTURE(Vec4Fixture, Vec4Add)
+{
+    a = b+c;
+    i = vec4_add(j,k);
+    CHECK_EQUAL_VEC4((float*)&a, (float*)&i);
+}
+TEST_FIXTURE(Vec4Fixture, Vec4Sub)
+{
+    a = b-c;
+    i = vec4_sub(j,k);
+    CHECK_EQUAL_VEC4((float*)&a, (float*)&i);
+}
+TEST_FIXTURE(Vec4Fixture, Vec4Mul)
+{
+    a = b*c;
+    i = vec4_mul(j,k);
+    CHECK_EQUAL_VEC4((float*)&a, (float*)&i);
+}
+TEST_FIXTURE(Vec4Fixture, Vec4Div)
+{
+    a = b/c;
+    i = vec4_div(j,k);
+    CHECK_EQUAL_VEC4((float*)&a, (float*)&i);
+}
+
+TEST_FIXTURE(Vec4Fixture, Vec4AddScalar)
+{
+    a = b+s;
+    i = vec4_add_scalar(j,s);
+    CHECK_EQUAL_VEC4((float*)&a, (float*)&i);
+}
+TEST_FIXTURE(Vec4Fixture, Vec4SubScalar)
+{
+    a = b-s;
+    i = vec4_sub_scalar(j,s);
+    CHECK_EQUAL_VEC4((float*)&a, (float*)&i);
+}
+TEST_FIXTURE(Vec4Fixture, Vec4MulScalar)
+{
+    a = b*s;
+    i = vec4_mul_scalar(j,s);
+    CHECK_EQUAL_VEC4((float*)&a, (float*)&i);
+}
+TEST_FIXTURE(Vec4Fixture, Vec4DivScalar)
+{
+    a = b/s;
+    i = vec4_div_scalar(j,s);
+    CHECK_EQUAL_VEC4((float*)&a, (float*)&i);
+}
+TEST_FIXTURE(Vec4Fixture, Vec4Hadd)
+{
+    float expected = a.x+a.y+a.z+a.w;
+    float actual = vec4_hadd(i);
+    CHECK_EQUAL_FLOAT(expected, actual);
+}
+TEST_FIXTURE(Vec4Fixture, Vec4Equal)
+{
+    CHECK_TRUE(vec4_equal(j, j));
+    CHECK_FALSE(vec4_equal(j, k));
+}
+TEST_FIXTURE(Vec4Fixture, Vec4Length)
+{
+    CHECK_EQUAL_FLOAT(glm::length(a), vec4_length(i));
+}
+TEST_FIXTURE(Vec4Fixture, Vec4Distance)
+{
+    CHECK_EQUAL_FLOAT(glm::distance(a,b), vec4_distance(i,j));
+    CHECK_EQUAL_FLOAT(vec4_distance(j,i), vec4_distance(i,j));
+}
+TEST_FIXTURE(Vec4Fixture, Vec4Normalize)
+{
+    a = glm::normalize(a);
+    i = vec4_normalize(i);
+    CHECK_EQUAL_VEC4((float*)&a, (float*)&i);
+}
+TEST_FIXTURE(Vec4Fixture, Vec4MinMax)
+{
+    a = glm::max(b, c);
+    i = vec4_max(j,k);
+    CHECK_EQUAL_VEC4((float*)&a, (float*)&i);
+    a = glm::min(b, c);
+    i = vec4_min(j,k);
+    CHECK_EQUAL_VEC4((float*)&a, (float*)&i);
+}
+TEST_FIXTURE(Vec4Fixture, Vec4Lerp)
+{
+    float f = _rand_float(0.0f, 1.0f);
+    a = glm_lerp(b,c,f);
+    i = vec4_lerp(j,k,f);
+    CHECK_EQUAL_VEC4((float*)&a, (float*)&i);
+}
+TEST_FIXTURE(Vec4Fixture, Vec4Negate)
+{
+    a = -b;
+    i = vec4_negate(j);
+    CHECK_EQUAL_VEC4((float*)&a, (float*)&i);
+}
+
+/******************************************************************************\
+ * Mat3                                                                       *
+\******************************************************************************/
+struct Mat3Fixture
+{
+    glm::mat3   a,b,c;
+    Mat3        i,j,k;
+    float       s;
+
+    Mat3Fixture()
+    {
+        float* _a = (float*)&a;
+        float* _b = (float*)&i;
+        float* end = _a + sizeof(a)/sizeof(float) * 3;
+        while(_a != end) {
+            *_b = *_a = _rand_float(-100.0f, 100.0f);
+            ++_a, ++_b;
+        }
+        s = _rand_float(-100.0f, 100.0f);
+    }
+    ~Mat3Fixture() { }
+};
+TEST_FIXTURE(Mat3Fixture, Mat3Identity)
+{
+    a = glm::mat3();
+    i = mat3_identity;
+    CHECK_EQUAL_MAT3((float*)&a, (float*)&i);
+}
+TEST_FIXTURE(Mat3Fixture, Mat3Scale)
+{
+    float x = _rand_float(0.1f, 100.0f),
+          y = _rand_float(0.1f, 100.0f),
+          z = _rand_float(0.1f, 100.0f);
+    a = (glm::mat3)glm::scale(glm::mat4(), glm::vec3(x,y,z));
+    i = mat3_scale(x,y,z);
+    CHECK_EQUAL_MAT3((float*)&a, (float*)&i);
+}
+IGNORE_TEST_FIXTURE(Mat3Fixture, Mat3Rotate)
+{
+    float x = _rand_float(0.1f, 100.0f),
+          y = _rand_float(0.1f, 100.0f),
+          z = _rand_float(0.1f, 100.0f);
+    a = (glm::mat3)glm::rotate(glm::mat4(), s, glm::vec3(x,y,z));
+    //i = mat3_rotate(x,y,z);
+    CHECK_EQUAL_MAT3((float*)&a, (float*)&i);
 }
 
 } // anonymous namespace
