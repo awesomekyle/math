bf5c3a670250606b2fde369003e361c352f33f52
Kyle Weicht <kyle@awesomekyle.com>
2017-11-14 21:26:54 -0600

    Vec4

--- include/akmath.h
+++ include/akmath.h
@@ -14,8 +14,17 @@ struct Vec3
     float y;
     float z;
 };
+struct Vec4
+{
+    float x;
+    float y;
+    float z;
+    float w;
+};
 
-// Vec2 math
+/*****************************************************************************\
+ * Vec2                                                                       *
+\*****************************************************************************/
 constexpr inline Vec2 operator+(Vec2 const a, Vec2 const b)
 {
     return {a.x + b.x, a.y + b.y};
@@ -33,13 +42,13 @@ constexpr inline Vec2 operator/(Vec2 const a, Vec2 const b)
     return {a.x / b.x, a.y / b.y};
 }
 
-constexpr inline Vec2 operator*(Vec2 const a, float const b)
+constexpr inline Vec2 operator*(Vec2 const a, float const f)
 {
-    return {a.x * b, a.y * b};
+    return {a.x * f, a.y * f};
 }
-constexpr inline Vec2 operator/(Vec2 const a, float const b)
+constexpr inline Vec2 operator/(Vec2 const a, float const f)
 {
-    return {a.x / b, a.y / b};
+    return {a.x / f, a.y / f};
 }
 
 // Vec2 misc
@@ -83,7 +92,9 @@ inline Vec2 operator-(Vec2 const v)
     return {-v.x, -v.y};
 }
 
-// Vec3 math
+/*****************************************************************************\
+ * Vec3                                                                       *
+\*****************************************************************************/
 constexpr inline Vec3 operator+(Vec3 const a, Vec3 const b)
 {
     return {a.x + b.x, a.y + b.y, a.z + b.z};
@@ -101,13 +112,13 @@ constexpr inline Vec3 operator/(Vec3 const a, Vec3 const b)
     return {a.x / b.x, a.y / b.y, a.z / b.z};
 }
 
-constexpr inline Vec3 operator*(Vec3 const a, float const b)
+constexpr inline Vec3 operator*(Vec3 const a, float const f)
 {
-    return {a.x * b, a.y * b, a.z * b};
+    return {a.x * f, a.y * f, a.z * f};
 }
-constexpr inline Vec3 operator/(Vec3 const a, float const b)
+constexpr inline Vec3 operator/(Vec3 const a, float const f)
 {
-    return {a.x / b, a.y / b, a.z / b};
+    return {a.x / f, a.y / f, a.z / f};
 }
 
 // Vec3 misc
@@ -162,6 +173,78 @@ inline Vec3 Cross(Vec3 const a, Vec3 const b)
         a.z * b.x - a.x * b.z,
         a.x * b.y - a.y * b.x,
     };
+}
+
+/*****************************************************************************\
+ * Vec4                                                                       *
+\*****************************************************************************/
+constexpr inline Vec4 operator+(Vec4 const a, Vec4 const b)
+{
+    return {a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w};
+}
+constexpr inline Vec4 operator-(Vec4 const a, Vec4 const b)
+{
+    return {a.x - b.x, a.y - b.y, a.z - b.z, a.w - b.w};
+}
+constexpr inline Vec4 operator*(Vec4 const a, Vec4 const b)
+{
+    return {a.x * b.x, a.y * b.y, a.z * b.z, a.w * b.w};
+}
+constexpr inline Vec4 operator/(Vec4 const a, Vec4 const b)
+{
+    return {a.x / b.x, a.y / b.y, a.z / b.z, a.w / b.w};
+}
+
+constexpr inline Vec4 operator*(Vec4 const a, float const f)
+{
+    return {a.x * f, a.y * f, a.z * f, a.w * f};
+}
+constexpr inline Vec4 operator/(Vec4 const a, float const f)
+{
+    return {a.x / f, a.y / f, a.z / f, a.w / f};
+}
+
+// Vec4 misc
+inline float LengthSq(Vec4 const a)
+{
+    return a.x * a.x + a.y * a.y + a.z * a.z + a.w * a.w;
+}
+inline float Length(Vec4 const a)
+{
+    return sqrtf(LengthSq(a));
+}
+inline float DistanceSq(Vec4 const a, Vec4 const b)
+{
+    return LengthSq(a - b);
+}
+inline float Distance(Vec4 const a, Vec4 const b)
+{
+    return sqrtf(DistanceSq(a, b));
+}
+inline Vec4 Normalize(Vec4 const v)
+{
+    float const length = Length(v);
+    return v / length;
+}
+inline Vec4 Min(Vec4 const a, Vec4 const b)
+{
+    return {a.x < b.x ? a.x : b.x, a.y < b.y ? a.y : b.y, a.z < b.z ? a.z : b.z,
+            a.w < b.w ? a.w : b.w};
+}
+inline Vec4 Max(Vec4 const a, Vec4 const b)
+{
+    return {a.x > b.x ? a.x : b.x, a.y > b.y ? a.y : b.y, a.z > b.z ? a.z : b.z,
+            a.w > b.w ? a.w : b.w};
+}
+inline Vec4 Lerp(Vec4 const a, Vec4 const b, float const t)
+{
+    auto const d = b - a;
+    return a + (d * t);
+}
+
+inline Vec4 operator-(Vec4 const v)
+{
+    return {-v.x, -v.y, -v.z, -v.w};
 }
 
 }  // namespace ak
--- test/math-test-dx.cpp
+++ test/math-test-dx.cpp
@@ -44,6 +44,22 @@ inline bool operator==(const ak::Vec3& k, const DirectX::XMVECTOR& x)
     return x == k;
 }
 
+// vec4
+DirectX::XMVECTOR XmFromAk(const ak::Vec4& v)
+{
+    return DirectX::XMVectorSet(v.x, v.y, v.z, v.w);
+}
+
+inline bool operator==(const DirectX::XMVECTOR& x, const ak::Vec4& k)
+{
+    return DirectX::XMVectorGetX(x) == Approx(k.x) && DirectX::XMVectorGetY(x) == Approx(k.y) &&
+           DirectX::XMVectorGetZ(x) == Approx(k.z) && DirectX::XMVectorGetW(x) == Approx(k.w);
+}
+inline bool operator==(const ak::Vec4& k, const DirectX::XMVECTOR& x)
+{
+    return x == k;
+}
+
 }  // namespace
 
 TEST_CASE("vec2 creation", "[vec2]")
@@ -283,5 +299,106 @@ TEST_CASE("vec3 arithmatic", "[vec3]")
     SECTION("cross")
     {
         REQUIRE(XMVector3Cross(a, b) == ak::Cross(i, j));
+    }
+}
+
+TEST_CASE("vec4 arithmatic", "[vec4]")
+{
+    ak::Vec4 const i = {
+        RandFloat(-50.0f, 50.0f),
+        RandFloat(-50.0f, 50.0f),
+        RandFloat(-50.0f, 50.0f),
+        RandFloat(-50.0f, 50.0f),
+    };
+    ak::Vec4 const j = {
+        RandFloat(-50.0f, 50.0f),
+        RandFloat(-50.0f, 50.0f),
+        RandFloat(-50.0f, 50.0f),
+        RandFloat(-50.0f, 50.0f),
+    };
+    ak::Vec4 const k = {
+        RandFloat(-50.0f, 50.0f),
+        RandFloat(-50.0f, 50.0f),
+        RandFloat(-50.0f, 50.0f),
+        RandFloat(-50.0f, 50.0f),
+    };
+
+    DirectX::XMVECTOR const a = XmFromAk(i);
+    DirectX::XMVECTOR const b = XmFromAk(j);
+    DirectX::XMVECTOR const c = XmFromAk(k);
+    float const scalar = RandFloat(-50.0f, 50.0f);
+
+    REQUIRE(a == i);
+    REQUIRE(b == j);
+    REQUIRE(c == k);
+
+    SECTION("plus")
+    {
+        auto const x = a + b;
+        auto const y = i + j;
+        REQUIRE(x == y);
+    }
+    SECTION("minus")
+    {
+        auto const x = a - b;
+        auto const y = i - j;
+        REQUIRE(x == y);
+    }
+    SECTION("times")
+    {
+        auto const x = a * b;
+        auto const y = i * j;
+        REQUIRE(x == y);
+    }
+    SECTION("divide")
+    {
+        auto const x = a / b;
+        auto const y = i / j;
+        REQUIRE(x == y);
+    }
+
+    SECTION("times scalar")
+    {
+        auto const x = a * scalar;
+        auto const y = i * scalar;
+        REQUIRE(x == y);
+    }
+    SECTION("divide scalar")
+    {
+        auto const x = a / scalar;
+        auto const y = i / scalar;
+        REQUIRE(x == y);
+    }
+
+    SECTION("length")
+    {
+        REQUIRE(XMVectorGetX(XMVector4Length(a)) == ak::Length(i));
+        REQUIRE(XMVectorGetX(XMVector4LengthSq(b)) == Approx(ak::LengthSq(j)));
+    }
+    SECTION("distance")
+    {
+        REQUIRE(XMVectorGetX(XMVector4Length(a - b)) == ak::Distance(i, j));
+        REQUIRE(ak::Distance(j, k) == ak::Distance(k, j));
+    }
+    SECTION("normalize")
+    {
+        REQUIRE(XMVector4Normalize(a) == ak::Normalize(i));
+    }
+
+    SECTION("min max")
+    {
+        REQUIRE(XMVectorMin(a, b) == ak::Min(i, j));
+        REQUIRE(XMVectorMax(a, b) == ak::Max(i, j));
+    }
+
+    SECTION("lerp")
+    {
+        float const f = RandFloat(0.0f, 1.0f);
+        REQUIRE(XMVectorLerp(a, b, f) == ak::Lerp(i, j, f));
+    }
+
+    SECTION("negate")
+    {
+        REQUIRE(-a == -i);
     }
 }
